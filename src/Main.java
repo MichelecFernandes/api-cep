c94e05657bf0a253d4b2a6e789bcbeccdb2f74e4
MichelecFernandes <michelecristinafr@gmail.com>
2024-10-28 12:35:09 -0300

    feat: fazendo a leitura do cep pelo o que o usuario digitar

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
         SearchCep searchCep = new SearchCep();
+        System.out.println("Digite  numero do cep a ser buscado:");
+        var cep = leitura.nextLine();
+
         try {
-            Address newAddress = searchCep.searchAddress("1");
+            Address newAddress = searchCep.searchAddress(cep);
             System.out.println(newAddress);
         } catch (RuntimeException e) {
             System.out.println(e.getMessage());
